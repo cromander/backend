6b1013fcb63fbae5696df41cf4e1737d3b26b72d
Adrian <adrian.arranz@outlook.com>
2025-03-26 14:39:20 +0100

    fix(backend): update S3 upload route to use new named s3_tools blueprint

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from routes.events import events_bp
 from routes.chats import chats_bp
 from routes.health import health_bp
 from database.db import init_db
-from routes.s3_upload import s3_upload_bp
+from routes.s3_tools import s3_tools_bp
 
 
 app = Flask(__name__)
@@ -17,7 +17,8 @@ app.register_blueprint(users_bp, url_prefix='/users')
 app.register_blueprint(events_bp, url_prefix='/events')
 app.register_blueprint(chats_bp, url_prefix='/chats')
 app.register_blueprint(health_bp, url_prefix='/health')
-app.register_blueprint(s3_upload_bp, url_prefix='/v1/s3')
+app.register_blueprint(s3_tools_bp, url_prefix='/v1/s3')
+
 
 if __name__ == '__main__':
     init_db()  
